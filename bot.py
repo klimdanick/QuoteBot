eefc3a318356a1b4e85557056e59dafd1ce17bf3
tijmenspeek <tijmen@speekenbrink.com>
2024-05-05 16:14:30 +0200

    cleaning up gross shit

--- bot.py
+++ bot.py
@@ -35,8 +35,8 @@ def daily_quote():
     with open('quotes.json', 'r') as f:
         date = dt.datetime.now().strftime("%d/%m/%Y")
         data = json.load(f)
-        print(date);
-        print(data);
+        print(date)
+        print(data)
         if len(data["quotes"]) == 0:
             return quote
         elif (data["metaData"]["time"] == date):
@@ -89,95 +89,95 @@ async def stats(interaction):
     with open('quotes.json', 'r') as f:
         data = json.load(f)
         totalQuotes = len(data["quotes"])
-        leaderBoard = {};
+        leaderBoard = {}
         
         for quote in data["quotes"]:
             if quote["auteur"].find("&") != -1:
-                auteurs = quote["auteur"].split(" & ");
+                auteurs = quote["auteur"].split(" & ")
                 for a in auteurs:
                     if not a.lower() in leaderBoard:
-                        leaderBoard[a.lower()] = 1;
+                        leaderBoard[a.lower()] = 1
                     else:
-                        leaderBoard[a.lower()] += 1;
-                continue;
+                        leaderBoard[a.lower()] += 1
+                continue
             if quote["auteur"].find(" en ") != -1:
-                auteurs = quote["auteur"].split(" en ");
+                auteurs = quote["auteur"].split(" en ")
                 for a in auteurs:
                     if not a.lower() in leaderBoard:
-                        leaderBoard[a.lower()] = 1;
+                        leaderBoard[a.lower()] = 1
                     else:
-                        leaderBoard[a.lower()] += 1;
-                continue;
+                        leaderBoard[a.lower()] += 1
+                continue
             if not quote["auteur"].lower() in leaderBoard:
-                leaderBoard[quote["auteur"].lower()] = 1;
+                leaderBoard[quote["auteur"].lower()] = 1
             else:
-                leaderBoard[quote["auteur"].lower()]+= 1;
+                leaderBoard[quote["auteur"].lower()]+= 1
          
         leaderBoard = dict(sorted(leaderBoard.items(),key=lambda x:x[1],reverse=True))
-        totalAuthors = len(leaderBoard);
-        colWidth = max(math.log(totalQuotes, 10), len("totalAuthors"));
-        cols = 2;
-        rows = 2;
-        board1 = makeBoard(colWidth, cols, rows);
-        board1 = setValueInBoard(board1, "totalQuotes", 0, 0, cols, colWidth);
-        board1 = setValueInBoard(board1, f"{totalQuotes}", 1, 0, cols, colWidth);
-        board1 = setValueInBoard(board1, "totalAuthors", 0, 1, cols, colWidth);
-        board1 = setValueInBoard(board1, f"{totalAuthors}", 1, 1, cols, colWidth);
-        #print(board1);
+        totalAuthors = len(leaderBoard)
+        colWidth = max(math.log(totalQuotes, 10), len("totalAuthors"))
+        cols = 2
+        rows = 2
+        board1 = makeBoard(colWidth, cols, rows)
+        board1 = setValueInBoard(board1, "totalQuotes", 0, 0, cols, colWidth)
+        board1 = setValueInBoard(board1, f"{totalQuotes}", 1, 0, cols, colWidth)
+        board1 = setValueInBoard(board1, "totalAuthors", 0, 1, cols, colWidth)
+        board1 = setValueInBoard(board1, f"{totalAuthors}", 1, 1, cols, colWidth)
+        #print(board1)
         
-        colWidth = 12;
-        cols = 3;
-        rows = 1+len(leaderBoard);
-        board2 = makeBoard(colWidth, cols, rows);
-        board2 = setValueInBoard(board2, "place", 0, 0, cols, colWidth);
-        board2 = setValueInBoard(board2, "auteur", 1, 0, cols, colWidth);
-        board2 = setValueInBoard(board2, "quotes", 2, 0, cols, colWidth);
-        i = 1;
+        colWidth = 12
+        cols = 3
+        rows = 1+len(leaderBoard)
+        board2 = makeBoard(colWidth, cols, rows)
+        board2 = setValueInBoard(board2, "place", 0, 0, cols, colWidth)
+        board2 = setValueInBoard(board2, "auteur", 1, 0, cols, colWidth)
+        board2 = setValueInBoard(board2, "quotes", 2, 0, cols, colWidth)
+        i = 1
         for auteur in leaderBoard:
-            board2 = setValueInBoard(board2, f"{i}", 0, i, cols, colWidth);
-            board2 = setValueInBoard(board2, auteur, 1, i, cols, colWidth);
-            board2 = setValueInBoard(board2, f"{leaderBoard[auteur]}", 2, i, cols, colWidth);
-            i+=1;
-        #print(board2);
+            board2 = setValueInBoard(board2, f"{i}", 0, i, cols, colWidth)
+            board2 = setValueInBoard(board2, auteur, 1, i, cols, colWidth)
+            board2 = setValueInBoard(board2, f"{leaderBoard[auteur]}", 2, i, cols, colWidth)
+            i+=1
+        #print(board2)
         await interaction.response.send_message(board1+board2)
     
 def setValueInBoard(board, value, x, y, cols, colWidth):
     label = chr((x)+(y*cols)+ord('{'))
     for i in range(len(value), colWidth):
-        value+=" ";
-    board = board.replace(label, value);
-    return board;
+        value+=" "
+    board = board.replace(label, value)
+    return board
   
 def makeBoard(colWidth, cols, rows):
-    board = "```\n┌";
+    board = "```\n┌"
     for j in range(cols):
         for i in range(colWidth):
-            board += "─";
+            board += "─"
         if not j == cols-1:
-            board += "┬";
-    board+= "┐\n";
+            board += "┬"
+    board+= "┐\n"
     for i in range(rows):
         for j in range(cols):
-            board += f"│{chr((j)+(i*cols)+ord('{'))}";
-        board += "│\n";
+            board += f"│{chr((j)+(i*cols)+ord('{'))}"
+        board += "│\n"
         
         if not i == rows-1:           
-            board += "├";
+            board += "├"
             for k in range(cols):
                 for l in range(colWidth):
-                    board += "─";
+                    board += "─"
                 if not k == cols-1:
-                    board += "┼";
-            board+= "┤\n";
+                    board += "┼"
+            board+= "┤\n"
         
-    board+= "└";
+    board+= "└"
     for j in range(cols):
         for i in range(colWidth):
-            board += "─";
+            board += "─"
         if not j == cols-1:
-            board += "┴";
-    board+= "┘\n```";
-    return board;
+            board += "┴"
+    board+= "┘\n```"
+    return board
     
 @client.event
 async def on_ready():
